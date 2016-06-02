67c2d2d3405cc098bedfdc50e04f730bfc3c150f
Cael Guargena <884264190@hs-61015-macair.ad.edenpr.org>
2016-06-02 12:36:34 -0500

    Added sprites and implemented character

--- NewestJavaGame/src/project/gfx/Assets.java
+++ NewestJavaGame/src/project/gfx/Assets.java
@@ -14,7 +14,13 @@ public class Assets {
 
 	public static void init(){
 		SpriteSheet sheet = new SpriteSheet(ImageLoader.loadImage("/textures/sheet.png"));
-		
+		SpriteSheet RotisserieL1 = new SpriteSheet(ImageLoader.loadImage("/textures/RM-WalkL.png"));
+		SpriteSheet RotisserieL2 = new SpriteSheet(ImageLoader.loadImage("/textures/RM-WalkL2.png"));
+		SpriteSheet RotisserieR1 = new SpriteSheet(ImageLoader.loadImage("/textures/RM-WalkR.png"));
+		SpriteSheet RotisserieR2 = new SpriteSheet(ImageLoader.loadImage("/textures/RM-WalkR2.png"));
+		SpriteSheet RotisserieIdle1 = new SpriteSheet(ImageLoader.loadImage("/textures/RM-IR.png"));
+		SpriteSheet RotisserieIdle2 = new SpriteSheet(ImageLoader.loadImage("/textures/RM-IR2.png"));
+
 		btn_start = new BufferedImage[2];
 		btn_start[0] = sheet.crop(width * 6, height * 4, width * 2, height);
 		btn_start[1] = sheet.crop(width * 6, height * 5, width * 2, height);
@@ -24,14 +30,14 @@ public class Assets {
 		player_left = new BufferedImage[2];
 		player_right = new BufferedImage[2];
 		
-		player_down[0] = sheet.crop(width * 4, 0, width, height);
-		player_down[1] = sheet.crop(width * 5, 0, width, height);
-		player_up[0] = sheet.crop(width * 6, 0, width, height);
-		player_up[1] = sheet.crop(width * 7, 0, width, height);
-		player_right[0] = sheet.crop(width * 4, height, width, height);
-		player_right[1] = sheet.crop(width * 5, height, width, height);
-		player_left[0] = sheet.crop(width * 6, height, width, height);
-		player_left[1] = sheet.crop(width * 7, height, width, height);
+		player_down[0] = RotisserieIdle1.crop(0, 0, width*2, height*2);
+		player_down[1] = RotisserieIdle2.crop(0, 0, width*2, height*2);
+		player_up[0] = RotisserieIdle1.crop(0, 0, width*2, height*2);
+		player_up[1] = RotisserieIdle2.crop(0, 0, width*2, height*2);
+		player_right[0] = RotisserieR1.crop(0, 0, width*2, height*2);
+		player_right[1] = RotisserieR2.crop(0, 0, width*2, height*2);
+		player_left[0] = RotisserieL1.crop(0, 0, width*2, height*2);
+		player_left[1] = RotisserieL2.crop(0, 0, width*2, height*2);
 		
 		zombie_down = new BufferedImage[2];
 		zombie_up = new BufferedImage[2];
@@ -70,3 +76,4 @@ public class Assets {
 	}
 	
 }
+
